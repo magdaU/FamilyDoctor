cf569ea3563ccc3fabcec0031ceb960fa8280a6b
Magda <s9199@pjwstk.edu.pl>
2018-10-13 10:47:10 +0200

    Initial first project and connect to database.

---
+++ src/main/java/pl/alburnus/familydoctor/FamilyDoctorApplication.java
@@ -0,0 +1,12 @@
+package pl.alburnus.familydoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyDoctorApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/alburnus/familydoctor/FamilyDoctorApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.alburnus.familydoctor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FamilyDoctorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
