3d4c0da0c64ee03de2fde7562e280f2fae75f556
magdaU <magdalena.ukleja@gmail.com>
2019-06-29 17:09:29 +0200

    Create two tests to patient delete method

---
+++ src/test/java/com/github/magdau/service/PatientServiceTest.java
@@ -0,0 +1,44 @@
+package com.github.magdau.service;
+
+import com.github.magdau.repository.PatientRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PatientServiceTest {
+
+    @InjectMocks
+    private PatientService patientService;
+
+    @Mock
+    private PatientRepository patientRepository;
+
+
+    @Test
+    public void whenPatientNotExists_thenDoNothing() {
+        // given
+        Mockito.when(patientRepository.existsById(1L)).thenReturn(false);
+
+        // when
+        patientService.delete(1L);
+
+        // then
+        Mockito.verify(patientRepository, Mockito.times(0)).deleteById(1L);
+    }
+
+    @Test
+    public void whenPatientExists_thenInvokeDelete() {
+        // given
+        Mockito.when(patientRepository.existsById(1L)).thenReturn(true);
+
+        // when
+        patientService.delete(1L);
+
+        // then
+        Mockito.verify(patientRepository, Mockito.times(1)).deleteById(1L);
+    }
+}
