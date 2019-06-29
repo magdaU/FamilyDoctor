024888f6b713696ad5e063ea8438057668c8d01e
magdaU <magdalena.ukleja@gmail.com>
2019-06-29 17:30:50 +0200

    Create two tests to patient update method

--- src/test/java/com/github/magdau/service/PatientServiceTest.java
+++ src/test/java/com/github/magdau/service/PatientServiceTest.java
@@ -1,5 +1,6 @@
 package com.github.magdau.service;
 
+import com.github.magdau.model.Patient;
 import com.github.magdau.repository.PatientRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +8,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PatientServiceTest {
@@ -40,5 +43,27 @@ public class PatientServiceTest {
 
         // then
         Mockito.verify(patientRepository, Mockito.times(1)).deleteById(1L);
+    }
+
+    @Test
+    public void whenPatientFound_thenUpdateIt() {
+        //given
+        Patient patient = new Patient();
+        patient.setId(2L);
+        Mockito.when(patientRepository.findById(patient.getId())).thenReturn(Optional.of(new Patient()));
+        //when
+        patientService.update(patient);
+        //then
+        Mockito.verify(patientRepository, Mockito.times(1)).save(patient);
+    }
+
+    @Test
+    public void whenPatientNotFound_thenDoNothing() {
+        //given
+        Mockito.when(patientRepository.findById(1L)).thenReturn(Optional.empty());
+        //when
+        patientService.update(new Patient());
+        //then
+        Mockito.verify(patientRepository, Mockito.times(0)).save(new Patient());
     }
 }
