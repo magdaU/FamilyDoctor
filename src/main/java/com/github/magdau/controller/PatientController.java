f610eb1c6eaa2330bffd946d87a56dad3af0e291
magdaU <magdalena.ukleja@gmail.com>
2019-07-22 14:14:25 +0200

    Edit patient

--- src/main/java/com/github/magdau/controller/PatientController.java
+++ src/main/java/com/github/magdau/controller/PatientController.java
@@ -2,12 +2,12 @@ package com.github.magdau.controller;
 
 import com.github.magdau.model.Patient;
 import com.github.magdau.service.PatientService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
 
 @Controller
 @RequestMapping(value = "/patients")
@@ -37,6 +37,20 @@ public class PatientController {
     @GetMapping("/delete/{patientId}")
     public String delete(@PathVariable("patientId") Long patientId) {
         patientService.delete(patientId);
+        return "redirect:/patients";
+    }
+
+    @GetMapping(value = "/edit/{id}")
+    public String patientEdit(Model theModel, @PathVariable("id") Long id) {
+        System.out.println(id);
+        Optional<Patient> patient = patientService.getById(id);
+        patient.ifPresent(p -> theModel.addAttribute("patient", p));
+        return "patientEdit";
+    }
+
+    @PostMapping("/save")
+    public String savePatient(@ModelAttribute("patient") Patient thePatient) {
+        patientService.save(thePatient);
         return "redirect:/patients";
     }
 }
