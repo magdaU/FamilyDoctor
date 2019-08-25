1c8c80a55e191fff3a4d915d44974966c794b287
magdaU <magdalena.ukleja@gmail.com>
2019-08-25 22:17:19 +0200

    Add annotation @Configuration

--- src/main/java/com/github/magdau/config/SecurityConfig.java
+++ src/main/java/com/github/magdau/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.github.magdau.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
